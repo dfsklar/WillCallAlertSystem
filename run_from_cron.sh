66e00efbc25a80b6d64b3eab31ee665e6ca91dd9
David F. Sklar <dfsklar@gmail.com>
2018-02-19 21:05:33 -0500

    Better cleanup of log dir

--- run_from_cron.sh
+++ run_from_cron.sh
@@ -1,3 +1,4 @@
-cd /root/WillCallAlertSystem/
+cd /home/dfsklar/WillCallAlertSystem/
 find ./logs  -name '*.out' -empty -print -exec /bin/rm {} \;
+find ./logs  -name '*.out' -mtime 14  -print -exec /bin/rm {} \;
 python main.py > logs/$$.out 2>&1
